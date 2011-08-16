0f1f351345bb0d53d402b9b8fe30e1d144b0439d
Abhimanyu Dhar <abhimanyudhar2005@gmail.com>
2011-08-16 00:53:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdmissionAdvisors::Application.config.secret_token = '4800ea7230b97095536a7caab2e583b558ec4ab4a4f3c25a216771e3e7198048ee67eb63cc7d5623b7d2118460d09c01d8094df1ecad86ceb8b100304ee9be07'
